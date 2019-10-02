1b3ecde924b5f431cb111cfc3666d0187aab410d
Natalia Shkarovska <45074266+natahiko@users.noreply.github.com>
2019-10-02 14:08:42 +0300

    Update workType.py

--- workType.py
+++ workType.py
@@ -1,7 +1,7 @@
 import re
 
-types = ['помилковий', 'слово', 'ціле число', 'дійсне число', 'ідентифікатор', 'дата', 'пошта', 'зарезервоване']
-reserved = ['shkarovska', 'sha', 'vas', 'rov', 'oka']
+types = ['помилковий', 'слово', 'ціле число', 'дійсне число', 'ідентифікатор', 'дата', 'пошта', 'зарезервоване','телефон']
+reserved = ['shkarovska', 'shka', 'vas', 'rov', 'oka']
 reserved2 = ['sin', 'cos', 'tg', 'ctg', 'break', 'continue', 'do', 'for']
 
 
@@ -14,7 +14,7 @@ def isLetter(char):
 
 
 def isSeparator(char):
-    separators = [' ', '(', ')', '"', '-', '!', '?', '\n']
+    separators = [' ', '(', ')', '"', '!', '?', '\n']
     for i in separators:
         if char == i: return True
     return False
@@ -44,32 +44,45 @@ def read_from_file(path):
     return f.read()
 
 
-def check_data(word):
+def check_data(word, origin):
     for i in range(len(word)):
         if word[i].isdigit():
             continue
         else:
             return 0
     if len(word) == 2 or len(word) == 4:
+        if int(origin[0]+origin[1])>32 or int(origin[3]+origin[4])>12:
+            return 0
         return 5
     return 0
 
 
-def check_real_data(word):
+def check_real_data(word, origin):
     for i in range(len(word)):
         if word[i] == '.' or word[i] == ',':
-            return check_data(word[(i + 1):])
+            return check_data(word[(i + 1):], origin)
         elif word[i].isdigit():
             continue
         else:
             return 0
     return 3
 
+def isTelephone(word):
+    if word[0]=="+":
+        word = word[1:]
+    phonePattern = re.compile(r'^(\d{3})(\d{3})(\d{4})$')
+    phonePattern2 = re.compile(r'^(\d{3})-(\d{3})-(\d{4})$')
+    phonePattern3 = re.compile(r'^(\d{2})(\d{3})(\d{3})(\d{4})$')
+    phonePattern4 = re.compile(r'^(\d{2})-(\d{3})-(\d{3})-(\d{4})$')
+    phonePattern5 = re.compile(r'^(\d{3})-(\d{2})-(\d{3})-(\d{4})$')
+    if phonePattern.match(word) or phonePattern2.match(word) or phonePattern3.match(word) or phonePattern4.match(word) or phonePattern5.match(word):
+        return True;
+    return False;
 
-def check_digit(word):
+def check_digit(word, origin):
     for i in range(len(word)):
         if word[i] == '.' or word[i] == ',':
-            return check_real_data(word[(i + 1):])
+            return check_real_data(word[(i + 1):], origin)
         elif word[i].isdigit():
             continue
         else:
@@ -90,11 +103,9 @@ def check_gmail(word, dog):
     last = True
     for i in word:
         if (i == '@' or i == '.') and last:
-            print(i + '1')
             return 0
         elif i == '@' and not last:
             if dog:
-                print(i + '2')
                 return 0
             else:
                 dog = True
@@ -104,7 +115,6 @@ def check_gmail(word, dog):
         elif isLetter(i):
             last = False
         else:
-            print(i + '3')
             return 0
     return 6
 
@@ -163,8 +173,10 @@ def print_word_type(word):
     if word == "": return
     if isLetter(word[0]):
         print(word + " - " + types[check_word(word)])
-    elif word[0].isdigit():
-        print(word + " - " + types[check_digit(word)])
+    elif isTelephone(word):
+        print(word + " - " + types[8])
+    elif word[0].isdigit() or word[0]=='+' or word[0]=='-':
+        print(word + " - " + types[check_digit(word[1:], word)])
     else:
         print(word + " - " + types[0])
 
